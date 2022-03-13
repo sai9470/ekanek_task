4a82c9d5fc0c80cb91f0677b1c94c28eec5c5c2d
Sk9470 <saikrishna.parimi@bacancy.com>
2022-03-13 23:43:24 +0530

    added basic setup and devise gem

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :current_password)
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    "http://localhost:3000/tets"
+  end
+end
